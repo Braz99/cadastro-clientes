42b1d385f325c355a39d6175d96f56b7e145ac57
braz99 <fabriciobrazil99@gmail.com>
2022-02-24 11:08:31 -0300

    Adiciona as funcionalidades do firebase

--- src/components/FormClient.tsx
+++ src/components/FormClient.tsx
@@ -26,7 +26,7 @@ export default function FormClient(props: FormProps) {
           color="blue"
           className="mr-10 w-40"
         >
-          {id ? "salvar" : "alterar"}
+          {id ? "alterar" : "salvar"}
         </ButtonApp>
         <ButtonApp className="w-40" onClick={props.cancel}>
           Cancelar
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,60 +7,32 @@ import TableClients from "../components/TableCients";
 import Client from "../core/Client";
 import ClientsRepository from "../core/ClientsRepository";
 
-// interface items {
-//   index: number;
-//   value: string;
-// }
-
-// let letters: Array<items> = [
-//   { index: 0, value: "e" },
-//   { index: 1, value: "a" },
-//   { index: 2, value: "g" },
-//   { index: 3, value: "h" },
-//   { index: 4, value: "i" },
-//   { index: 5, value: "v" },
-//   { index: 6, value: "z" },
-//   { index: 7, value: "b" },
-//   { index: 8, value: "x" },
-//   { index: 9, value: "y" },
-// ];
-
 export default function Home() {
-  // function l() {
-  //   let number = Math.trunc(Math.random() * 10);
-  //   let [b] = letters.filter((item) => item.index === number);
-  //   return b;
-  // }
-
-  // function generateID() {
-  //   let letter = l();
-  //   return (
-  //     Math.trunc(Math.random() * 1000 + Math.random() * 500) + letter?.value
-  //   );
-  // }
   let [clientSelected, setClientSelected] = useState<Client>(Client.empty());
   let [visible, setVisible] = useState<"formulary" | "table">("table");
   let [clients, setClients] = useState<Client[]>([]);
 
   let repo: ClientsRepository = new ClientCollection();
-  // let clients = [
-  //   new Client("Ana", 17, 1),
-  //   new Client("Beatriz", 19, 2),
-  //   new Client("Joaquim", 20, 3),
-  //   new Client("Manuela", 21, 4),
-  // ];
 
   useEffect(() => {
-    repo.showAllClients().then(setClients);
+    showClients;
   }, []);
+
+  function showClients() {
+    repo.showAllClients().then((clients) => {
+      setClients(clients);
+      setVisible("table");
+    });
+  }
 
   function selectClient(client: Client) {
     setClientSelected(client);
     setVisible("formulary");
   }
 
-  function removeClient(client: Client) {
-    console.log(client.name, "removed");
+  async function removeClient(client: Client) {
+    await repo.removeClient(client);
+    showClients();
   }
 
   function newClient() {
@@ -68,8 +40,9 @@ export default function Home() {
     setVisible("formulary");
   }
 
-  function saveClient(client: Client) {
-    console.log(client);
+  async function saveClient(client: Client) {
+    await repo.saveClient(client);
+    showClients();
   }
 
   return (
